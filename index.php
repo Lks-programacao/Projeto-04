aadcd05e51b53fd47dd894de93a18cad4915ef57
Lks.Programação <124909098+Lks-programacao@users.noreply.github.com>
2023-04-14 15:07:59 -0300

    Projeto 4
    > Esse projeto é uma segunda versão do meu primeiro site, sendo mais complexo e melhor

---
+++ index.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="scss/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="scss/style.css">
+  	<link rel="stylesheet" type="text/css" href="scss/slick.css">
+
+	<title></title>
+
+</head>
+<body>
+
+	<section class="header">
+		<div class="container">
+			<a href="#"><div class="header-logo"></div><!--header logo--></a>
+
+			<div class="header-icon"></div><!--header icon-->
+
+			<div class="clear"></div><!--clear-->
+
+		</div><!--container-->
+	</section><!--header-->
+
+	<section class="header-menu">
+		<div class="container">
+			<nav class="desktop">
+				
+				<ul>
+					<li><span></span><a href="#">HOME</a></li>
+					<li><span></span><a href="#">CONTATO</a></li>
+					<li><span></span><a href="#">OFERTAS</a></li>
+					<li><span></span><a href="#">MASCULINO</a></li>
+					<li><span></span><a href="#">FEMININO</a></li>
+				</ul>
+
+			</nav><!--DESKTOP-->
+
+
+			<nav class="mobile">
+
+					<h3><i class="fa fa-bars"></i></h3>
+				
+				<ul>
+					<li><span></span><a href="#">HOME</a></li>
+					<li><span></span><a href="#">CONTATO</a></li>
+					<li><span></span><a href="#">OFERTAS</a></li>
+					<li><span></span><a href="#">MASCULINO</a></li>
+					<li><span></span><a href="#">FEMININO</a></li>
+				</ul>
+
+			</nav><!--MOBILE-->
+
+
+			<div class="header-pesquisa">
+
+					<img src="imagens/icon/lupapesquisa.png">
+
+				<input type="text" name="pesquisa" placeholder="Procure aqui...">
+			</div><!--header pesquisa-->
+
+
+			<div class="clear"></div><!--clear-->
+
+		</div>
+
+	</section><!--header-menu-->
+
+	<section class="section-bg">
+
+	</section><!--section bg-->
+
+	
+	<section class="diferenciais">
+		<div class="container">
+			
+			<div class="diferenciais-single">
+
+					<img src="imagens/icon/entrega-rapida.png">
+
+				<h3 style="padding-bottom: ;">Frete <br class="br"> Grátis!</h3>
+
+			</div><!--diferenciais single-->
+
+			<div class="diferenciais-single">
+
+					<img src="imagens/icon/cartao.png">
+
+				<h3>Diversos meios de pagamento!</h3>
+
+			</div><!--diferenciais single-->
+
+			<div class="diferenciais-single">
+
+					<img src="imagens/icon/desconto.png">
+
+				<h3 style="padding-bottom: ;" >Até 30% <br class="br"> de desconto!</h3>
+
+			</div><!--diferenciais single-->
+
+			<div class="diferenciais-single">
+
+					<img style="max-width: px;" src="imagens/icon/escudo.png">
+
+				<h3 style="padding-bottom: ;" >Compra <br class="br"> 100% segura!</h3>
+
+			</div><!--diferenciais single-->
+
+
+
+
+		</div><!--container-->
+	</section><!--diferencias-->
+
+	<section class="destaque">
+		<div class="container">
+
+			<div class="destaque-texto texto">
+				<h2>Destaques</h2>
+				<h2><i class="fa fa-angle-down"></i></h2>
+			</div>
+
+			<div class="destaque-box">
+				
+				<div class="box-single">
+					<h3>Air Jordan</h3>
+				</div><!--box-single-->
+
+				<div class="box-single">
+					<h3>Air <BR>Max</h3>
+				</div><!--box-single-->
+
+				<div class="box-single">
+					<h3>Air Force</h3>
+				</div><!--box-single-->
+
+			</div><!--destaque-box-->
+
+
+		</div><!--container-->
+	</section><!--destaque-->
+
+	<section class="section-bg2">
+	</section><!--section bg2-->
+
+	<section class="loja">
+		<div class="container">
+
+			<div class="loja-texto texto">
+				<h2>Nossa Loja</h2>
+				<h2><i class="fa fa-angle-down"></i></h2>
+			</div><!--loja-texto-->
+
+				<div class="loja-container off">
+					
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis2.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis1.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis3.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis4.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis5.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis6.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis8.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis9.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+
+				</div><!--loja container-->
+
+				<div  class="loja-container off loja-slide">
+					
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis2.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis1.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis3.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis4.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis5.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis6.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis8.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+					<div class="loja-single">
+						
+							<img src="imagens/png/tenis9.png">
+
+						<div class="loja-nome">
+							<h3>Air Jordan</h3>
+							<p>R$ 1.129</p>
+						</div><!--loja nome-->
+
+					</div><!--loja single-->
+
+
+				</div><!--loja container-->
+
+					<button>Ver mais</button>
+
+		</div><!--container-->
+	</section><!--loja-->
+
+	<footer>
+		<div class="container">
+			
+					<img src="imagens/logo/Logo.png">	
+				<h2>Meu Site</h2>
+
+		</div><!--container-->
+
+		<div class="footer2">
+				
+			<h3>Todos os direitos reservados - <a target="_blank" href="https://www.lks-programacao.com">www.lks-programacao.com</a> </h3>
+
+		</div><!--footer2-->
+	</footer>
+	
+
+	<script src="js/jquery.js"></script>
+	<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+	<script src="js/slides.js"></script>
+
+	<script>
+		
+		$('.mobile i').click(function(){
+
+			$('.mobile ul').slideToggle();
+		})
+
+	</script>
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.diferenciais .container').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  arrows: false,
+		  centerMode: true,
+		  autoplaySpeed: 2000,
+		  responsive:[
+
+		  	{breakpoint: 1114,
+
+		  	settings:{
+		  	arrows: false,
+		  	slidesToShow: 2,
+		  	centerMode: true
+		  	}
+		  },
+
+		  	{breakpoint: 778,
+		  	settings:{
+		  		arrows: false,
+		  		slidesToShow: 1,
+		  		centerMode: true
+		  	}}
+
+		  	]
+	});
+
+$('.container .loja-slide').slick({arrows: false, centerpadding: true, slidesToShow: 1});
+		
+		
+    });
+  </script>
+	
+
+
+</body>
+</html>
